369b24e7f5cde684f9d98124c6cf50b2ae88ed2c
craig-lion <craig.m.lindahl@gmail.com>
2023-04-05 18:51:24 -0700

    creating pillbox containers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,30 +7,33 @@ const inter = Inter({ subsets: ['latin'] });
 
 interface PillBoxProps {
   title: string;
+  container: React.FC<{}>;
 }
-<div></div>
-const PillBox: React.FC<PillBoxProps> = ({title}) => {
+
+const PillBox: React.FC<PillBoxProps> = ({title, container: Container}) => {
   return(
     <div>
       <div className={styles.pillBoxTopTitle}>{title}</div>
-      <div className={styles.pillBoxBottomContainer}>Butt Stuff Here</div>
+      <div className={styles.pillBoxBottomContainer}>
+        Butt Stuff Here
+        <Container/>
+      </div>
     </div>
   );
 };
 
-const PillBoxBottomContainer: React.FC = () => {
+const PillBoxBottomContainer: React.FC<{}> = () => {
   return (
-    <div>
+    <div className={styles.pillBoxBottomContainer}>
       <div>InfoBoxComponent1</div>
       <div>InfoBoxComponent2</div>
     </div>
   );
 };
-const InfoBox: React.FC = () => {
+const InfoBox: React.FC<{}> = () => {
   return (
     <div>
       <div>InfoBoxComponent1</div>
-      <div>InfoBoxComponent2</div>
     </div>
   );
 };
@@ -46,19 +49,19 @@ export default function Home() {
       </div>
       <div className={styles.mainContainer}>
         <div className={styles.pillBox}>
-          <PillBox title="WELCOME TO CROWTOWN FAMILY!"/>
+          <PillBox title="WELCOME TO CROWTOWN FAMILY!" container={PillBoxBottomContainer} />
         </div>
         <div className={styles.pillBox}>
-          <PillBox title="MEET DEB SHES DOPE!"/>
+          <PillBox title="MEET DEB SHES DOPE!" container={PillBoxBottomContainer}/>
         </div>
         <div className={styles.pillBox}>
-          <PillBox title="MADE WITH LOVE AND CARE AND SKILL AND STUFF!"/>
+          <PillBox title="MADE WITH LOVE AND CARE AND SKILL AND STUFF!" container={PillBoxBottomContainer}/>
         </div>
         <div className={styles.pillBox}>
-          <PillBox title="THESE ITEMS ARE DELUXE!"/>
+          <PillBox title="THESE ITEMS ARE DELUXE!" container={PillBoxBottomContainer}/>
         </div>
         <div className={styles.pillBox}>
-          <PillBox title="OUR MYSPACE IS FLY!"/>
+          <PillBox title="OUR MYSPACE IS FLY!" container={PillBoxBottomContainer}/>
         </div>
       </div>
     </>
